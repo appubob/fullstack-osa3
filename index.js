9d696998091a3530ffb30046bc51682ca12410da
appubob <anttiv2002@gmail.com>
2024-05-09 14:46:23 +0300

    osa3 3.1-3.21

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -38,14 +40,10 @@ app.get('/info', async (request, response) => {
   response.send(`<p>Phonebook has info for ${count} people </br> ${Date()}</p>`)
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
-  } else if (!body.number) {
+  if (!body.number) {
     return response.status(400).json({ 
       error: 'number missing' 
     })
@@ -87,15 +85,13 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
-  console.log(body)
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
